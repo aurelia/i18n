e8c0d357c3f65255fbb2912a418ca7bab40ccca6
Rob Eisenberg <rob@bluespire.com>
2015-06-26 23:13:55 -0400

    chore(all): setup repo

---
+++ build/babel-options.js
@@ -0,0 +1,19 @@
+module.exports = {
+  filename: '',
+  filenameRelative: '',
+  modules: '',
+  sourceMap: true,
+  sourceMapName: '',
+  sourceRoot: '',
+  moduleRoot: '',
+  moduleIds: false,
+  experimental: false,
+  comments: false,
+  compact: false,
+  code:true,
+  stage:2,
+  loose: "all",
+  optional: [
+    "es7.decorators"
+  ]
+};
---
+++ config.js
@@ -0,0 +1,31 @@
+System.config({
+  "transpiler": "babel",
+  "babelOptions": {
+    "optional": [
+      "runtime",
+      "es7.decorators"
+    ]
+  },
+  "paths": {
+    "*": "*.js",
+    "github:*": "jspm_packages/github/*.js",
+    "npm:*": "jspm_packages/npm/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "babel": "npm:babel-core@5.1.13",
+    "babel-runtime": "npm:babel-runtime@5.1.13",
+    "core-js": "npm:core-js@0.9.5",
+    "github:jspm/nodelibs-process@0.1.1": {
+      "process": "npm:process@0.10.1"
+    },
+    "npm:core-js@0.4.10": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:core-js@0.9.5": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    }
+  }
+});
