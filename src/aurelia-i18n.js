cfb058f5627cb25e31eed760d1b9503ab8648bfb
Zewa <zewa666@gmail.com>
2016-01-09 12:15:48 +0100

    fix(params): add missing htmlParamsResource
     adds the missing htmlParamsResource for automatic t-params alias registration

--- src/aurelia-i18n.js
+++ src/aurelia-i18n.js
@@ -34,6 +34,7 @@ function configure(frameworkConfig, cb) {
   frameworkConfig.postTask(() => {
     let resources = frameworkConfig.container.get(ViewResources);
     let htmlBehaviorResource = resources.getAttribute('t');
+    let htmlParamsResource   = resources.getAttribute('t-params');
     let attributes = instance.i18next.options.attributes;
 
     // Register default attributes if none provided
