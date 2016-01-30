3374c2076e9790f301aafb3c883c49fae813a779
Rob Eisenberg <rob@bluespire.com>
2016-01-30 14:53:26 -0500

    chore(config): fix up to latest

--- config.js
+++ config.js
@@ -8,14 +8,13 @@ System.config({
     ]
   },
   paths: {
-    "aurelia-i18n/*": "dist\\system/*js",
     "github:*": "jspm_packages/github/*",
     "npm:*": "jspm_packages/npm/*"
   },
 
   map: {
     "Intl.js": "github:andyearnshaw/Intl.js@0.1.4",
-    "aurelia-binding": "npm:aurelia-binding@1.0.0-beta.1.1.0",
+    "aurelia-binding": "npm:aurelia-binding@1.0.0-beta.1.1.1",
     "aurelia-dependency-injection": "npm:aurelia-dependency-injection@1.0.0-beta.1.1.2",
     "aurelia-event-aggregator": "npm:aurelia-event-aggregator@1.0.0-beta.1.1.0",
     "aurelia-loader-default": "npm:aurelia-loader-default@1.0.0-beta.1.1.1",
@@ -43,7 +42,7 @@ System.config({
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
     },
-    "npm:aurelia-binding@1.0.0-beta.1.1.0": {
+    "npm:aurelia-binding@1.0.0-beta.1.1.1": {
       "aurelia-metadata": "npm:aurelia-metadata@1.0.0-beta.1.1.3",
       "aurelia-pal": "npm:aurelia-pal@1.0.0-beta.1.1.1",
       "aurelia-task-queue": "npm:aurelia-task-queue@1.0.0-beta.1.1.0",
@@ -79,7 +78,7 @@ System.config({
       "aurelia-pal": "npm:aurelia-pal@1.0.0-beta.1.1.1"
     },
     "npm:aurelia-templating-resources@1.0.0-beta.1.1.0": {
-      "aurelia-binding": "npm:aurelia-binding@1.0.0-beta.1.1.0",
+      "aurelia-binding": "npm:aurelia-binding@1.0.0-beta.1.1.1",
       "aurelia-dependency-injection": "npm:aurelia-dependency-injection@1.0.0-beta.1.1.2",
       "aurelia-loader": "npm:aurelia-loader@1.0.0-beta.1.1.0",
       "aurelia-logging": "npm:aurelia-logging@1.0.0-beta.1.1.1",
@@ -90,7 +89,7 @@ System.config({
       "core-js": "npm:core-js@2.0.3"
     },
     "npm:aurelia-templating@1.0.0-beta.1.1.0": {
-      "aurelia-binding": "npm:aurelia-binding@1.0.0-beta.1.1.0",
+      "aurelia-binding": "npm:aurelia-binding@1.0.0-beta.1.1.1",
       "aurelia-dependency-injection": "npm:aurelia-dependency-injection@1.0.0-beta.1.1.2",
       "aurelia-loader": "npm:aurelia-loader@1.0.0-beta.1.1.0",
       "aurelia-logging": "npm:aurelia-logging@1.0.0-beta.1.1.1",
