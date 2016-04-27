9678d50fb72df7be70bcee74bc132ce8e9aadd55
vsoftic <vsoftic@ranorex.com>
2016-04-27 11:13:12 +0200

    fix(df-vc): changed signature of DfValueConverter
    the signature of the DfValueConverter got changed, still maintaining the
    original support. Additionaly for the old mode a console.warn deprecation
    notice will be logged.
    
    Fixes issue: https://github.com/aurelia/i18n/issues/100

--- src/df.js
+++ src/df.js
@@ -5,10 +5,16 @@ export class DfValueConverter {
   constructor(i18n) {
     this.service = i18n;
   }
-
-  toView(value, formatOptions, locale, dateFormat) {
-    let df = dateFormat || this.service.df(formatOptions, locale || this.service.getLocale());
-
-    return df.format(value);
+  
+  toView(value, dfOrOptions, locale, df) {
+    if (dfOrOptions && (typeof dfOrOptions.format === 'function')) {
+      return dfOrOptions.format(value);
+    } else if(df) {
+      console.warn("This ValueConverter signature is depcrecated and will be removed in future releases. Please use the signature [dfOrOptions, locale]");
+      return df.format(value);
+    } else {
+      df = this.service.df(dfOrOptions, locale || this.service.getLocale());
+      return df.format(value);
+    }
   }
 }
