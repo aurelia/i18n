c2b54d36aff23bae794887bbb1da949fa15db221
Andrew <Muntyan>
2016-05-02 02:20:57 +0300

    i18next.d.ts and i18next-xhr-backend.d.ts has moved to /doc folder. Added some instruction about library with typescript to README

---
+++ doc/i18next-xhr-backend.d.ts
@@ -0,0 +1,27 @@
+declare module 'i18next-xhr-backend' {
+
+    interface Interpolator {
+        interpolate: () => string
+    }
+    interface Services {
+        interpolator: Interpolator
+    }
+    export class Backend {
+        type: 'backend';
+        services: Services;
+        options: {
+            loadPath: string,
+            addPath:  string,
+            allowMultiLoading: boolean,
+            parse: () => {},
+            crossDomain: boolean,
+            ajax: () => void
+        };
+        constructor(services: Services, options?: {});
+        init(services: Services, options?: {}): void;
+        readMulti(languages: Array, namespaces: Array, callback: () => void): void;
+        read(language: {}, namespace: {}, callback: () => void): void;
+        loadUrl(url: string, callback: () => void): void;
+        create(languages: any[], namespace: string, key: string, fallbackValue: string): void;
+    }
+}
