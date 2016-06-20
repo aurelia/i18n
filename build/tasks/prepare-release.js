5a2d611ede1062893fcabe06b0d94b4a6056092d
Rob Eisenberg <rob@bluespire.com>
2016-06-19 20:48:34 -0400

    chore(build): update conventional change log config

--- build/tasks/prepare-release.js
+++ build/tasks/prepare-release.js
@@ -1,15 +1,18 @@
 var gulp = require('gulp');
 var runSequence = require('run-sequence');
 var paths = require('../paths');
-var changelog = require('conventional-changelog');
 var fs = require('fs');
 var bump = require('gulp-bump');
 var args = require('../args');
+var conventionalChangelog = require('gulp-conventional-changelog');
 
-gulp.task('bump-version', function(){
-  return gulp.src(['./package.json', './bower.json'])
-    .pipe(bump({type:args.bump })) //major|minor|patch|prerelease
-    .pipe(gulp.dest('./'));
+gulp.task('changelog', function () {
+  return gulp.src(paths.doc + '/CHANGELOG.md', {
+    buffer: false
+  }).pipe(conventionalChangelog({
+    preset: 'angular'
+  }))
+  .pipe(gulp.dest(paths.doc));
 });
 
 gulp.task('changelog', function(callback) {
